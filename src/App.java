0ec43e3a84019cd03264c491ab7da76b680fa1ed
hessede <hgodlove@yahoo.com>
2023-01-20 09:44:59 -0500

    Succeed to make the rechercheBinaire work while returning a index instead of the value

--- src/App.java
+++ src/App.java
@@ -1,10 +1,13 @@
 import laboratoire1.RechercheTableau;
 
+import java.util.Arrays;
+
 public class App {
 
     public static void main(String[] args) {
         RechercheTableau rech = new RechercheTableau();
-        int[] tab = new int[]{1, 2};
-        System.out.println(rech.RechercheBinaire(tab, 2, 1));
+        int[] tab = new int[]{10, 24, 12, 36, 17, 48, 23, 8};
+        System.out.println(rech.RechercheLineaire(tab, tab.length, 17));
+        System.out.println(rech.RechercheBinaire(tab, tab.length, 17));
     }
 }
--- src/laboratoire1/RechercheTableau.java
+++ src/laboratoire1/RechercheTableau.java
@@ -1,19 +1,46 @@
 package laboratoire1;
 
+import java.util.Arrays;
+
 public class RechercheTableau{
 
     // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme
     // Elles peuvent cependant servir seulement d'interface et utiliser une méthode "helper"
     // avec des paramètres supplémentaires, au besoin.
     public int RechercheLineaire(int[] tab, int n, int x){
+        for(int i = 0; i < n; i++){
+            if(tab[i] == x){
+                return i;
+            }
+        }
         return -1;
     }
 
     public int RechercheBinaire(int[] tab, int n, int val){
-        return -1;
+        if(n == tab.length){
+            n = 0;
+        }
+
+        if(tab.length == 1){
+            if(tab[0] == val){
+                return 0;
+            }else return -1;
+        }
+
+        int mid = (tab.length + n)/ 2;
+        if(tab[mid] == val){
+            return mid;
+        }
+
+        if(tab[mid] > val){
+            int[] tabLeft = Arrays.copyOfRange(tab, 0, mid);
+            return RechercheBinaire(tabLeft, n, val);
+        }
+        return RechercheBinaire(tab, mid, val);
     }
 
     public int RechercheBinaireModifie(int[] tab, int n, int val){
+
         return -1;
     }
 
