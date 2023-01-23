c9cede7a5b05f982808105b44ddb96bc65e55959
hessede <hgodlove@yahoo.com>
2023-01-23 17:02:00 -0500

    Amélioration des algorithmes

--- src/laboratoire1/RechercheTableau.java
+++ src/laboratoire1/RechercheTableau.java
@@ -1,78 +1,74 @@
 package laboratoire1;
 
-import java.util.Arrays;
 
-public class RechercheTableau{
+public class RechercheTableau {
 
     // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme
     // Elles peuvent cependant servir seulement d'interface et utiliser une méthode "helper"
     // avec des paramètres supplémentaires, au besoin.
-    public int RechercheLineaire(int[] tab, int n, int x){
-        for(int i = 0; i < n; i++){
-            if(tab[i] == x){
+    public int RechercheLineaire(int[] tab, int n, int x) {
+
+        for (int i = 0; i < n; i++) {
+            if (tab[i] == x) {
                 return i;
             }
         }
         return -1;
     }
 
-    public int RechercheBinaire(int[] tab, int n, int val){
-        if(n == tab.length){
-            n = 0;
+    public int RechercheBinaire(int[] tab, int n, int val) {
+        return RechercheBinaireHelper(tab, 0, n - 1, val);
+    }
+
+    private int RechercheBinaireHelper(int[] tab, int indexDepart, int indexFin, int val) {
+
+        if (indexDepart == indexFin) {
+            if (tab[indexDepart] == val) {
+                return indexDepart;
+            }
+            return -1;
         }
 
-        if(tab.length == 1){
-            if(tab[0] == val){
-                return 0;
-            }else return -1;
+        int indexMilieu = (indexDepart + indexFin) / 2;
+
+        if (tab[indexMilieu] > val) {
+            return RechercheBinaireHelper(tab, indexDepart, indexMilieu - 1, val);
+        } else if (tab[indexMilieu] < val) {
+            return RechercheBinaireHelper(tab, indexMilieu + 1, indexFin, val);
         }
 
-        int mid = (tab.length + n)/ 2;
-        if(tab[mid] == val){
-            return mid;
-        }
+        return indexMilieu;
 
-        if(tab[mid] > val){
-            int[] tabLeft = Arrays.copyOfRange(tab, 0, mid);
-            return RechercheBinaire(tabLeft, n, val);
-        }
-        return RechercheBinaire(tab, mid, val);
     }
 
     public int RechercheBinaireModifie(int[] tab, int n, int val) {
-
-        if (n == tab.length) {
-            n = 0;
-        }
-
-        if (tab.length == 1) {
-            if (tab[0] == val) {
-                return 0;
-            } else return -1;
-        }
-
-        int firstThird = (tab.length + n) / 3;
-        int secondThird = ((tab.length + n) * 2) / 3;
-
-        if(secondThird >= tab.length){
-            secondThird = tab.length - 1;
-        }
-
-        if (tab[firstThird] == val) {
-            return firstThird;
-        } else if (tab[secondThird] == val) {
-            return secondThird;
-        }
-
-        if (tab[firstThird] > val) {
-            int[] tabFirstThird = Arrays.copyOfRange(tab, 0, firstThird);
-            return RechercheBinaireModifie(tabFirstThird, n, val);
-        } else if (tab[firstThird] < val && tab[secondThird] > val){
-            int[] tabSecondThird = Arrays.copyOfRange(tab, 0, secondThird);
-            return RechercheBinaireModifie(tabSecondThird, firstThird, val);
-        }
-
-        return RechercheBinaireModifie(tab, secondThird, val);
+        return RechercheBinaireModifieHelper(tab, 0, n - 1, val);
     }
 
+    private int RechercheBinaireModifieHelper(int[] tab, int indexDepart, int indexFin, int val) {
+        if (indexFin == indexDepart) {
+            if (tab[indexDepart] == val) {
+                return indexDepart;
+            }
+            return -1;
+
+        } else {
+            int sectionTri = (indexFin - indexDepart) / 3;
+            int indexTier1 = indexDepart + sectionTri;
+            int indexTier2 = indexDepart + 2 * sectionTri;
+
+            if (tab[indexTier1] == val) {
+                return indexTier1;
+            } else if (tab[indexTier1] > val) {
+                return RechercheBinaireModifieHelper(tab, indexDepart, indexTier1 - 1, val);
+            }
+
+            if (tab[indexTier2] == val) {
+                return indexTier2;
+            } else if (tab[indexTier2] > val) {
+                return RechercheBinaireModifieHelper(tab, indexTier1 + 1, indexTier2 - 1, val);
+            }
+            return RechercheBinaireModifieHelper(tab, indexTier2 + 1, indexFin, val);
+        }
+    }
 }
