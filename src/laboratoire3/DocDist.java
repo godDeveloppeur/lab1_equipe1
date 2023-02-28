a5ec6e1ad4916400d51618659a23ef07239271c1
hessede <hgodlove@yahoo.com>
2023-02-28 10:09:52 -0500

    Creation du laboratoire 3

---
+++ src/laboratoire3/DocDist.java
@@ -0,0 +1,13 @@
+package laboratoire3;
+
+
+
+public class DocDist{
+
+    // Ne pas changer cette fonction, elle sera utilisée pour tester votre programme
+    public double docDistance(String nomFichier1, String nomFichier2)
+    {
+        return 0.0;
+    }
+
+}
